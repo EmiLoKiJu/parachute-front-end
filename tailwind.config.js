516f8c7e1d631253b4d2eac9734b35baf31bec56
Ali-0111 <alirahanet@gmail.com>
2023-10-26 10:27:57 +0430

    font and color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,14 @@ export default {
       },
       colors: {
         'brand_color': '#F9B809',
+        'brand_green': '#99C110',
+        'brand_orange': '##FFC600',
+        'brand_blue': '#10BBB5'
       },
-      // fontFamily: {
-      //   pop: ['pop', 'sans-serif'],
-      //   mont: ['Montserrat-Regular', 'sans-serif'],
-      // },
+      fontFamily: {
+        roboto: ['roboto', 'sans'],
+        // mont: ['Montserrat-Regular', 'sans-serif'],
+      },
       // backgroundImage:{
       //   'hero' : 'url(@/assets/images/hero/hero-1.jpg)',
       //   // 'hero2' : 'url(@/assets/images/hero/hero-1.jpg)',
