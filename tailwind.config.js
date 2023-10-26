8debddefa0da3e9e96a4c1730d39652a3b026dea
Ali-0111 <alirahanet@gmail.com>
2023-10-26 09:35:36 +0430

    Custom config lines added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "@/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        center: true,
+      },
+      colors: {
+        'brand_color': '#F9B809',
+      },
+      // fontFamily: {
+      //   pop: ['pop', 'sans-serif'],
+      //   mont: ['Montserrat-Regular', 'sans-serif'],
+      // },
+      // backgroundImage:{
+      //   'hero' : 'url(@/assets/images/hero/hero-1.jpg)',
+      //   // 'hero2' : 'url(@/assets/images/hero/hero-1.jpg)',
+      // },
+      // screens:{
+      //   'xs': '480px',
+      //   'lg': '992px',
+      // }
+    },
   },
   plugins: [],
 }
