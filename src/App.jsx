94b427f9e7e475811b4090696bf8d6b87fd85b02
dazehinn <dazehinn@yahoo.fr>
2023-10-31 18:31:09 +0100

    Update App to include roots

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Router from "@/routing/Router"
-
-
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Reservation from './components/content/reservation';
 function App() {
+  let isAuthorized = true;
   return (
-      <Router />
+    <div className="flex w-full overflow-x-hidden">
+      <Routes>
+        <Route path="/" element={<Reservation authorized={isAuthorized} />} />
+        <Route path="/reserve" element={<Reservation authorized={isAuthorized} />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
