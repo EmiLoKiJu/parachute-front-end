07ffda103806c7a8bef6c738aa4732bd566c9482
Ali-0111 <alirahanet@gmail.com>
2023-11-02 22:50:49 +0430

    Logo and humberger button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Aside from "@/components/aside/Aside";
+import Humberger_btn from '@/components/Humberger_btn';
 
 export default function Home() {
   return(
-    <div className="h-screen flex">
-      <div className="aside_container w-[15%] border border-r-1">
+    <div className="min-h-screen flex relative">
+      <div className="aside_container absolute z-10 w-[150px] h-full hidden lg:h-screen lg:block lg:w-[15%] lg:static border border-r-1">
         <Aside/>
       </div>
-      <div className="main_wrapper w-[85%] border border-red-300 flex flex-col justify-center">
-        <main className="home_sub_pages border">
+      {/* opacity-20 will be added */}
+      <div className="main_wrapper w-[85%] mx-auto flex flex-col justify-center">
+        <main className="home_sub_pages">
             <Outlet/>
         </main>
       </div>
-      
+      <Humberger_btn/>
     </div>
   );
 }
