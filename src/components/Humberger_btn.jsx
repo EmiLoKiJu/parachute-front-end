d37a39b3e585e5c463a5f42a90eff914a68f2371
Ali-0111 <alirahanet@gmail.com>
2023-11-03 10:51:42 +0430

    Added icons for close and open

--- src/components/Humberger_btn.jsx
+++ src/components/Humberger_btn.jsx
@@ -1,8 +1,10 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { setOnOff } from "@/redux/Humberger/humbergerSlice.js"
-import { FaWindowClose } from 'react-icons/fa'
+import { FaWindowClose, FaGripLines } from 'react-icons/fa'
 
 export default function Humberger_btn() {
+  const { isClose } = useSelector((store) => (store.humberger_switch))
+
   const dispatch = useDispatch();
   const switchHandler = () => {
     dispatch(setOnOff())
@@ -12,7 +14,10 @@ export default function Humberger_btn() {
     <button className="z-20 absolute mx-5 my-4 p-1 w-[40px] lg:hidden hover:text-brand_green"
             onClick={switchHandler}
     >
-      <FaWindowClose className="w-full h-full"/>
+      {
+        isClose ? <FaGripLines className="w-full h-full"/>
+        : <FaWindowClose className="w-full h-full"/>
+      }
     </button>
   );
 }
